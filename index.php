6f98b7f9c88d54553c4ed55e8ca0786bda2cf0de
Pmnmpr <pmnmpr@gmail.com>
2021-12-20 07:37:03 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             61c0278e18924             |
+    |             61c0326a9eb84             |
     |_______________________________________|
 */
  use Pmpr\Package\Jalali\Jalali; Jalali::symcgieuakksimmu();
--- src/Pmpr/Package/Jalali/Container.php
+++ src/Pmpr/Package/Jalali/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             61c0278e18924             |
+    |             61c0326a9eb84             |
     |_______________________________________|
 */
  namespace Pmpr\Package\Jalali; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Common\Foundation\Manipulate\Post\ManipulatePost; class Container extends BaseClass { public function __construct() { $this->settingObj = Setting::symcgieuakksimmu(); parent::__construct(); } public function moeogyisgeuwoeea() : bool { return ManipulatePost::uqwgsuysegkweago($this->weysguygiseoukqw(Setting::VIRASTAR_POST_TYPE_TARGETS, [])); } }
