98d45ac95dae3f13105d7af5c3e29d53385b1ecd
Pmnmpr <pmnmpr@gmail.com>
2021-12-21 16:11:53 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             61c066e416054             |
+    |             61c1fca1edbee             |
     |_______________________________________|
 */
  use Pmpr\Package\Jalali\Jalali; Jalali::symcgieuakksimmu();
