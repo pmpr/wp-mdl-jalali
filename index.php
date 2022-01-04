f814bc705682c3cfe98920749472049ba2e42fba
Pmnmpr <pmnmpr@gmail.com>
2022-01-04 14:53:54 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             61c1fca1edbee             |
+    |             61d45f4c37226             |
     |_______________________________________|
 */
  use Pmpr\Package\Jalali\Jalali; Jalali::symcgieuakksimmu();
