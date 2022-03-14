83b632d116e2d0289e1bda9f94eae8486d303e4c
Pmnmpr <pmnmpr@gmail.com>
2022-03-14 23:03:53 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             61eed2022288b             |
+    |             622fc9aee3eb3             |
     |_______________________________________|
 */
  use Pmpr\Package\Jalali\Jalali; Jalali::symcgieuakksimmu();
--- src/Pmpr/Package/Jalali/Container.php
+++ src/Pmpr/Package/Jalali/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             61eed2022288b             |
+    |             622fc9aee3eb3             |
     |_______________________________________|
 */
  namespace Pmpr\Package\Jalali; use Pmpr\Common\Foundation\Container\Container as BaseClass; use Pmpr\Common\Foundation\Manipulate\Post\ManipulatePost; class Container extends BaseClass { public function __construct() { $this->settingObj = Setting::symcgieuakksimmu(); parent::__construct(); } public function moeogyisgeuwoeea() : bool { return ManipulatePost::uqwgsuysegkweago($this->weysguygiseoukqw(Setting::mequmaoyiwsmgmsi, [])); } }
