6db7b1dcb3349c5097cb3ffd942d0ef53ededfe4
pmpr.dev <pmpr.dev@gmail.com>
2023-07-15 13:10:43 +0000

    Obfuscated

--- src/Pmpr/Module/Jalali/Plugin/Common.php
+++ src/Pmpr/Module/Jalali/Plugin/Common.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6492e85959d13             |
+    |             64b29aa9897c4             |
     |_______________________________________|
 */
  namespace Pmpr\Module\Jalali\Plugin; use Pmpr\Module\Jalali\Container; abstract class Common extends Container { }
--- src/Pmpr/Module/Jalali/Plugin/Plugin.php
+++ src/Pmpr/Module/Jalali/Plugin/Plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6492e85959d13             |
+    |             64b29aa9897c4             |
     |_______________________________________|
 */
- namespace Pmpr\Module\Jalali\Plugin; class Plugin extends Common { public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\160\x6c\x75\x67\151\156\x73\x5f\154\157\141\144\x65\x64", [$this, "\x69\x65\155\x61\141\153\147\x71\147\x71\x6f\163\x69\145\143\x6d"]); } public function iemaakgqgqosiecm() { if (!$this->caokeucsksukesyo()->wikusamwomuogoau()->ggocakcisguuokai()) { goto cecuyayqoioasumi; } Woocommerce::symcgieuakksimmu(); cecuyayqoioasumi: } }
+ namespace Pmpr\Module\Jalali\Plugin; class Plugin extends Common { public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\x70\x6c\x75\147\x69\156\x73\x5f\154\157\x61\x64\x65\144", [$this, "\151\x65\155\141\141\x6b\147\161\x67\x71\157\163\151\145\x63\x6d"]); } public function iemaakgqgqosiecm() { if (!$this->caokeucsksukesyo()->wikusamwomuogoau()->ggocakcisguuokai()) { goto uiosisocuwokwkie; } Woocommerce::symcgieuakksimmu(); uiosisocuwokwkie: } }
