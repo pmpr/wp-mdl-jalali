1c78f41484443a83162140278d83a2e51319381d
pmpr.dev <pmpr.dev@gmail.com>
2024-08-19 18:12:49 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66c31d4c77962             |
+    |             66c38af84622c             |
     |_______________________________________|
 */
  use Pmpr\Module\Jalali\Jalali; Jalali::symcgieuakksimmu();
--- src/Pmpr/Module/Jalali/Plugin/Plugin.php
+++ src/Pmpr/Module/Jalali/Plugin/Plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66c31d4c77962             |
+    |             66c38af84622c             |
     |_______________________________________|
 */
- namespace Pmpr\Module\Jalali\Plugin; class Plugin extends Common { public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\160\x6c\165\x67\x69\156\163\x5f\x6c\x6f\x61\x64\x65\144", [$this, "\x69\143\x77\x63\147\x6d\x63\x6f\151\155\x71\145\x69\147\x79\145"]); } public function icwcgmcoimqeigye() { if (!$this->caokeucsksukesyo()->wikusamwomuogoau()->ggocakcisguuokai()) { goto cuumeogeomowqisc; } Woocommerce::symcgieuakksimmu(); cuumeogeomowqisc: } }
+ namespace Pmpr\Module\Jalali\Plugin; class Plugin extends Common { public function wigskegsqequoeks() { $this->qcsmikeggeemccuu("\160\154\x75\147\151\156\x73\x5f\154\x6f\x61\x64\x65\144", [$this, "\x69\143\x77\143\147\155\143\157\x69\x6d\161\x65\151\147\x79\x65"]); } public function icwcgmcoimqeigye() { if (!$this->caokeucsksukesyo()->wikusamwomuogoau()->ggocakcisguuokai()) { goto cuumeogeomowqisc; } Woocommerce::symcgieuakksimmu(); cuumeogeomowqisc: } }
